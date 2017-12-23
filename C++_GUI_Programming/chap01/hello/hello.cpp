303b14a6e4ebd3f47c3c101068e6ea065839ad81
chenshijie <xingwozhonghua@126.com>
2017-12-23 20:22:58 +0800

    change hello.cpp

--- C++_GUI_Programming/chap01/hello/hello.cpp
+++ C++_GUI_Programming/chap01/hello/hello.cpp
@@ -3,8 +3,12 @@
 
 int main(int argc, char *argv[])
 {
+    //创建一个 QApplication 对象
     QApplication app(argc, argv);
+    //QLabel 对象，QLabel 是一个 Qt 提供的小控件，显示一行文本
     QLabel *label = new QLabel("Hello Qt!");
+    //显示 QLabel。
     label->show();
+    //：QApplication.exec()，让程序进入消息循环。
     return app.exec();
 }
